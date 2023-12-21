afea8484b9ddcb43db99203f72269dc3edf0ff8b
niko dittmar <nikodittmar@gmail.com>
2023-12-21 13:48:33 -0800

    Initial Commit

---
+++ MyRaceTimer/MyRaceTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyRaceTimerApp.swift
+//  MyRaceTimer
+//
+//  Created by Niko Dittmar on 12/21/23.
+//
+
+import SwiftUI
+
+@main
+struct MyRaceTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
